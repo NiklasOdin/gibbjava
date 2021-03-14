aba2c0cead6b03df6269b9262246974816efbab1
Olof <olof.ohrn@gmail.com>
2021-03-14 17:17:59 +0100

    hh

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
-const name = document.getElementById('name')
+
 const form = document.getElementById('form')
 const errorElement = document.getElementById('error')
 
 form.addEventListener('submit', (e) => {
     let messages = []
-    if (name.value === '' || name.value == null){
+    if (form.name.value === '' || form.name.value == null){
         messages.push('Namn är obligatoriskt')
     }
 
     if (messages.length > 0) {
+        
         e.preventDefault()
         errorElement.innerText = messages.join(', ')
     }
-   
+
 })
+let pos = 0;
+function rollDiceMove() {
+    const elold = document.getElementById('pos' + (pos + 1)); 
+    elold.innerHTML = ''
+    const steps = Math.trunc(Math.random() * 6) + 1;
+    pos = (pos + steps)%28;
+    const el = document.getElementById('pos' + (pos + 1));
+    if (el) {
+        el.innerHTML = '<p>hej</p>';
+    }
+    const diceRoll = document.getElementById('diceRoll');
+    diceRoll.innerHTML = String('You rolled: ' + steps);
+}
+
